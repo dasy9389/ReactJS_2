b874c2d482daedfdf6e6db56ca38dba460ef1fca
dasy9389 <89760726+dasy9389@users.noreply.github.com>
2021-09-10 13:34:10 +0700

    Router - Menu

---
+++ router/src/Contact.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Contact extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <div>
+                <h2>Contact</h2>
+            </div>
+        );
+    }
+}
+export default Contact;
