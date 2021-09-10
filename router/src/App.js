445cb94dab2b83078bcb7d92540dfe8674cc6e61
dasy9389 <89760726+dasy9389@users.noreply.github.com>
2021-09-10 15:49:35 +0700

    createRef

--- router/src/App.js
+++ router/src/App.js
@@ -5,58 +5,32 @@ class App extends React.Component{
   constructor(props){
     super(props);
     this.state = { 
-      data: [],
-      data2: ''
+      data: ''
     };
-
-    // this.setStateHandler = this.setStateHandler.bind(this);
-  }
-  setStateHandler = () =>{
-    const item = 'SetState...';
-    // const myArray = this.state.data.slice(); //slice như là copy
-    // myArray.push(item);
-    // const myArray = [this.state.data];
-    const myArray = [...this.state.data];
-    myArray.push(item);
-    this.setState({data: myArray}); // thay doi du lieu thi goi state update lai du lieu
-  }
-
-  forceUpdateHandler = () => {
-    this.forceUpdate(); //cho dù ko có thay đổi nhưng vẫn ép update lại (render lại)
-  }
-
-  findDomNodeHandler = () => {
-    const myDiv = document.getElementById('myDiv');
-    reactDom.findDOMNode(myDiv).style.color = 'green';
-    //findDOMNode kiem tra xem co class do ko?
+    this.myInput = React.createRef();
   }
 
   updateState = (e) => {
-    this.setState({data2 : e.target.value}); //target.value la lay du lieu khi ngta nhap vo
+    this.setState({data : e.target.value}); //target.value la lay du lieu khi ngta nhap vo
   }
 
   clearInput = () => {
-    this.setState({data2:''});
-    reactDom.findDOMNode(this.refs.myInput).focus();
-    // refs khuyen cao ko sai nua vi co the bi loi, nen sai useRef() hoac createRef()
+    this.setState({data:''});
+    // reactDom.findDOMNode(this.refs.myInput).focus();
+    this.myInput.current.focus();
+  }
+
+  componentDidMount(){
+    this.myInput.current.value = 'Nhap vao...';
+    this.myInput.current.focus(); //load trang nhay vao input lien...
   }
 
   render(){
     return(
       <div>
-        <button onClick={this.setStateHandler}>SET STATE</button>
-        <h4>State Array: {this.state.data}</h4>
-
-        <button onClick={this.forceUpdateHandler}>SET STATE</button>
-        <h4>Random number: {Math.random()}</h4>
-
-        <button onClick={this.findDomNodeHandler}>FIND DOM</button>
-        <div id='myDiv'>NODE</div>
-
-        <br /><br /><br />
-        <input value={this.state.data2} onChange={this.updateState} ref='myInput'></input>
+        <input value={this.state.data} onChange={this.updateState} ref={this.myInput}></input>
         <button onClick={this.clearInput}>CLEAR</button>
-        <h4>{this.state.data2}</h4>
+        <h4>{this.state.data}</h4>
       </div>
     );
   }
