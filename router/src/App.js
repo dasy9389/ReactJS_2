05c38aed27bac9f0421549057b764afedfed6482
dasy9389 <89760726+dasy9389@users.noreply.github.com>
2021-09-10 15:14:54 +0700

    update componentapi

--- router/src/App.js
+++ router/src/App.js
@@ -1,15 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-import Menu from './Menu';
+import React from "react";
+import reactDom from "react-dom";
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = { data: [] };
+    // this.setStateHandler = this.setStateHandler.bind(this);
+  }
+  setStateHandler = () =>{
+    const item = 'SetState...';
+    // const myArray = this.state.data.slice(); //slice như là copy
+    // myArray.push(item);
+    // const myArray = [this.state.data];
+    const myArray = [...this.state.data];
+    myArray.push(item);
+    this.setState({data: myArray}); // thay doi du lieu thi goi state update lai du lieu
+  }
 
-        <Menu />
+  forceUpdateHandler = () => {
+    this.forceUpdate(); //cho dù ko có thay đổi nhưng vẫn ép update lại (render lại)
+  }
 
-    </div>
-  );
+  findDomNodeHandler = () => {
+    const myDiv = document.getElementById('myDiv');
+    reactDom.findDOMNode(myDiv).style.color = 'green';
+    //findDOMNode kiem tra xem co class do ko?
+  }
+
+  render(){
+    return(
+      <div>
+        <button onClick={this.setStateHandler}>SET STATE</button>
+        <h4>State Array: {this.state.data}</h4>
+
+        <button onClick={this.forceUpdateHandler}>SET STATE</button>
+        <h4>Random number: {Math.random()}</h4>
+
+        <button onClick={this.findDomNodeHandler}>FIND DOM</button>
+        <div id='myDiv'>NODE</div>
+
+      </div>
+    );
+  }
 }
-
 export default App;
